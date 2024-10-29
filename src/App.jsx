2237ca1cc6ba211e1cb723dd7fae48165a442783
Simon Ekström <simmeh_@live.se>
2024-10-29 14:23:05 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Brands from './components/Brands'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Brands />
+      <Features />
+      <Footer />
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div className="footer">
+            &copy; 2024 Silicon. All rights reserved.
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
